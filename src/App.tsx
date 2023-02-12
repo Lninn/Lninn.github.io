3c405b772857306e37af01701403bd3974382b4c
Lninn <thl384458870@outlook.com>
2023-02-12 15:50:41 +0800

    feat: add header

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import { useState } from 'react'
+
 import Card from './components/Card'
 import Header from './components/Header'
+
+import * as dayjs from 'dayjs'
+
+import clsx from 'clsx'
+
+// import 'dayjs/locale/zh-cn'
+// dayjs.locale('zh-cn')
 
 import './App.less'
 
@@ -13,11 +22,55 @@ const COLORS = [
   '#9d6bce',
 ]
 
+const dayOfWeek = dayjs().format('dddd')
+const dateOfMonth = dayjs().format('D')
+
+const dateList = [
+  dateOfMonth,
+  '13',
+  '14',
+  '15',
+  '16',
+]
+
 function App() {
+
+  const [activeDate, setActiveDate] = useState(dateOfMonth)
+
   return (
     <div className="App">
 
      <Header />
+
+     <div className='panel'>
+        <div className='datetime'>
+            <div className='dayOfWeek'>
+              {dayOfWeek}
+            </div>
+            <div className='dateOfMonth'>
+              {dateOfMonth}
+            </div>
+        </div>
+
+        <div className="panel__list">
+          {dateList.map(date => {
+            const handleClick = () => {
+              setActiveDate(date)
+            }
+            const cls = clsx(
+              'panel__list__item',
+              { active: date === activeDate }
+            )
+            const label = date === dateOfMonth ? 'TODAY' : date
+
+            return (
+              <div key={date} className={cls} onClick={handleClick}>
+                {label}
+              </div>
+            )
+          })}
+        </div>
+     </div>
 
       <Card title='DESIGN MEETING' bgColor={COLORS[0]} />
       <Card title='DAILY PROJECT' bgColor={COLORS[1]} />
