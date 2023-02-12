ea50ec827a14726859432505a4f260dfd305e30a
Lninn <thl384458870@outlook.com>
2023-02-12 16:01:51 +0800

    feat: update

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,6 @@
-import { useState } from 'react'
-
 import Card from './components/Card'
 import Header from './components/Header'
-
-import * as dayjs from 'dayjs'
-
-import clsx from 'clsx'
-
-// import 'dayjs/locale/zh-cn'
-// dayjs.locale('zh-cn')
+import DatePanel from './components/DatePanel'
 
 import './App.less'
 
@@ -22,56 +14,11 @@ const COLORS = [
   '#9d6bce',
 ]
 
-const dayOfWeek = dayjs().format('dddd')
-const dateOfMonth = dayjs().format('D')
-
-const dateList = [
-  dateOfMonth,
-  '13',
-  '14',
-  '15',
-  '16',
-]
-
 function App() {
-
-  const [activeDate, setActiveDate] = useState(dateOfMonth)
-
   return (
     <div className="App">
-
-     <Header />
-
-     <div className='panel'>
-        <div className='datetime'>
-            <div className='dayOfWeek'>
-              {dayOfWeek}
-            </div>
-            <div className='dateOfMonth'>
-              {dateOfMonth}
-            </div>
-        </div>
-
-        <div className="panel__list">
-          {dateList.map(date => {
-            const handleClick = () => {
-              setActiveDate(date)
-            }
-            const cls = clsx(
-              'panel__list__item',
-              { active: date === activeDate }
-            )
-            const label = date === dateOfMonth ? 'TODAY' : date
-
-            return (
-              <div key={date} className={cls} onClick={handleClick}>
-                {label}
-              </div>
-            )
-          })}
-        </div>
-     </div>
-
+      <Header />
+      <DatePanel />
       <Card title='DESIGN MEETING' bgColor={COLORS[0]} />
       <Card title='DAILY PROJECT' bgColor={COLORS[1]} />
       <Card title='WEEKLY PLANING' bgColor={COLORS[4]} />
---
+++ src/components/DatePanel.tsx
@@ -0,0 +1,60 @@
+import { useState } from 'react'
+import * as dayjs from 'dayjs'
+
+import clsx from 'clsx'
+
+// import 'dayjs/locale/zh-cn'
+// dayjs.locale('zh-cn')
+
+import './DatePanel.less'
+
+
+const dayOfWeek = dayjs().format('dddd')
+const dateOfMonth = dayjs().format('D')
+
+const dateList = [
+  dateOfMonth,
+  '13',
+  '14',
+  '15',
+  '16',
+]
+
+
+const DatePanel = () => {
+  const [activeDate, setActiveDate] = useState(dateOfMonth)
+
+  return (
+    <div className='panel'>
+      <div className='datetime'>
+          <div className='dayOfWeek'>
+            {dayOfWeek}
+          </div>
+          <div className='dateOfMonth'>
+            {dateOfMonth}
+          </div>
+      </div>
+
+      <div className="panel__list">
+        {dateList.map(date => {
+          const handleClick = () => {
+            setActiveDate(date)
+          }
+          const cls = clsx(
+            'panel__list__item',
+            { active: date === activeDate }
+          )
+          const label = date === dateOfMonth ? 'TODAY' : date
+
+          return (
+            <div key={date} className={cls} onClick={handleClick}>
+              {label}
+            </div>
+          )
+        })}
+      </div>
+  </div>
+  )
+}
+
+export default DatePanel
