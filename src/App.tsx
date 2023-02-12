dc8ab9b2f9d5185a29a08ceebd7ff983f938c21f
Lninn <thl384458870@outlook.com>
2023-02-12 10:23:23 +0800

    feat: add card

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,22 @@ import Card from './components/Card'
 import './App.css'
 
 
+const COLORS = [
+  '#bcef4b',
+  '#fef752',
+  '#1f1f1f',
+  '#b32580',
+  '#ffffff',
+  '#9d6bce',
+]
+
 function App() {
   return (
     <div className="App">
-      <h2>Hello World</h2>
 
-      <Card />
+      <Card title='DESIGN MEETING' bgColor={COLORS[0]} />
+      <Card title='DAILY PROJECT' bgColor={COLORS[1]} />
+      <Card title='WEEKLY PLANING' bgColor={COLORS[4]} />
     </div>
   )
 }
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,10 +1,45 @@
 import './Card.less'
 
 
-const Card = () => {
+interface CardProps {
+  title: string
+  bgColor: string
+}
+
+const Card = ({ title, bgColor }: CardProps) => {
   return (
-    <div className='card'>
-      Card
+    <div className='card' style={{ backgroundColor: bgColor }}>
+      <div className='left'>
+        <div className='time'>
+          <div className='hour'>
+            11
+          </div>
+          <div className='minute'>
+            30
+          </div>
+        </div>
+
+        <div className='line'></div>
+
+        <div className='time'>
+          <div className='hour'>
+            11
+          </div>
+          <div className='minute'>
+            30
+          </div>
+        </div>
+      </div>
+
+      <div className='card__content'>
+        {title}
+
+        <div className='footer'>
+          <div>NANA</div>
+          <div>ALEX</div>
+          <div>MARk</div>
+        </div>
+      </div>
     </div>
   )
 }
