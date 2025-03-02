ee5a85ffae4598f5b109ba7c8bdeb23c3b374098
Lninn <thl384458870@outlook.com>
2025-03-02 17:37:30 +0800

    添加问题方案提示链接

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // https://stackoverflow.com/questions/73459654/import-markdown-files-dynamically-with-vite
   assetsInclude: ['**/*.md'],
   define: {
     // Some libraries use the global object, even though it doesn't exist in the browser.
