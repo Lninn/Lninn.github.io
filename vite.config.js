a7c4e9f3b787b2e5d05534fbbaf205fdbe9c8626
Lninn <thl384458870@outlook.com>
2025-03-02 17:36:47 +0800

    修复构建报错问题

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  assetsInclude: ['**/*.md'],
   define: {
     // Some libraries use the global object, even though it doesn't exist in the browser.
     // Alternatively, we could add `<script>window.global = window;</script>` to index.html.
