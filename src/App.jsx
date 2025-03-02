3f3e817d838d81b04953875ad549337f243c5b19
Lninn <thl384458870@outlook.com>
2025-03-02 17:31:52 +0800

    修改文本加载方式

--- src/App.jsx
+++ src/App.jsx
@@ -11,19 +11,40 @@ import "highlight.js/styles/github.css"; // 引入代码高亮样式
 function App() {
   const [count, setCount] = useState(0)
 
+  const [files, setFiles] = useState([]);
   const [markdown, setMarkdown] = useState("");
 
   useEffect(() => {
-    // 加载 public 目录中的 init.md 文件
-    fetch("/init.md")
-      .then((response) => response.text())
-      .then((text) => setMarkdown(text))
-      .catch((error) => console.error("Failed to load markdown file:", error));
+
+     // 使用 import.meta.glob 获取文件列表
+     const fileList = import.meta.glob("/public/articles/*");
+     const fileNames = Object.keys(fileList).map((filePath) =>
+       filePath.split("/").pop()
+     );
+     setFiles(fileNames);
+
   }, []);
+
+  function loadFile(name) {
+     // 加载 public 目录中的 init.md 文件
+     fetch(`/articles/${name}`)
+     .then((response) => response.text())
+     .then((t) => {
+      console.log(t)
+       return t
+     })
+     .then((text) => setMarkdown(text))
+     .catch((error) => console.error("Failed to load markdown file:", error));
+  }
 
   return (
     <>
       <h1>Hello world</h1>
+
+      <p>
+        {files.map(name => (<a key={name} onClick={() => loadFile(name)}>{name}</a>))}
+      </p>
+
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
