1795c4aad6333eb80158d4a93ff7b1d61769b024
Lninn <thl384458870@outlook.com>
2025-03-03 19:11:14 +0800

    json数据整理

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import './App.css'
@@ -10,13 +10,16 @@ import rehypeHighlight from "rehype-highlight";
 import "highlight.js/styles/github.css"; // 引入代码高亮样式
 
 
-
 function App() {
 
   const [files, setFiles] = useState([]);
   const [markdown, setMarkdown] = useState("");
 
-  const [urlList, seturlList] = useState([])
+  var [urlData, setUrlData] = useState([])
+
+  useEffect(() => {
+    loadUrlList()
+  }, [])
 
   function getFiles() {
     // 使用 import.meta.glob 获取文件列表
@@ -45,9 +48,28 @@ function App() {
       .then((response) => response.text())
       .then((list) => {
         try {
-          seturlList(
-            JSON.parse(list)
-          )
+
+          var originalData = JSON.parse(list)
+          var restList = []
+
+          var dataMap = {}
+
+          for (const item of originalData) {
+            if ('category' in item) {
+              var name = item['category']
+              if (name in dataMap) {
+                dataMap[name].push(item)
+              } else {
+                dataMap[name] = [item]
+              }
+            } else {
+              restList.push(item)
+            }
+          }
+
+          var dataList = Object.entries(dataMap)
+          setUrlData(sortByFirstLetter(dataList, false))
+          console.log({ restList })
         } catch (error) {
           console.log(error)
         }
@@ -59,13 +81,50 @@ function App() {
     <>
       <p>
         <button onClick={() => getFiles()}>点击获取文件列表</button>
-        <button onClick={() => loadUrlList()}>加载URL list</button>
       </p>
 
       <CSVtoJSONConverter />
 
-      <ul>
-        {urlList.map(obj => {
+      <p>
+        {files.map(name => (<a key={name} onClick={() => loadFile(name)}>{name}</a>))}
+      </p>
+
+      <ReactMarkdown rehypePlugins={[rehypeHighlight]}>
+        {markdown}
+      </ReactMarkdown>
+
+      <section style={{ display: 'flex', flexWrap: 'wrap' }}>
+        {urlData.map(obj => {
+          var [name, data] = obj
+
+          return (
+            <div key={name}>
+              <span style={{ textTransform: 'uppercase' }}>{name}</span>
+              <UrlList list={data} />
+            </div>
+          )
+        })}
+      </section>
+
+      <p>power by Deepseek</p>
+    </>
+  )
+}
+
+function sortByFirstLetter(arr, ascending = true) {
+  return arr.sort((a, b) => {
+      if (ascending) {
+          return a[0][0].localeCompare(b[0][0]);
+      } else {
+          return b[0][0].localeCompare(a[0][0]);
+      }
+  });
+}
+
+function UrlList({ list }) {
+  return (
+    <ul>
+        {list.map(obj => {
           return (
             <li key={obj.name} style={{ marginBlockEnd: 8 }}>
               <img
@@ -79,17 +138,6 @@ function App() {
           )
         })}
       </ul>
-
-      <p>
-        {files.map(name => (<a key={name} onClick={() => loadFile(name)}>{name}</a>))}
-      </p>
-
-      <ReactMarkdown rehypePlugins={[rehypeHighlight]}>
-        {markdown}
-      </ReactMarkdown>
-
-      <p>power by Deepseek</p>
-    </>
   )
 }
 
