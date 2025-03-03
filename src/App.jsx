6276aeafa5cb9488852102b83255e3ca9054dae7
Lninn <thl384458870@outlook.com>
2025-03-03 09:31:46 +0800

    样式更新

--- src/App.jsx
+++ src/App.jsx
@@ -19,40 +19,40 @@ function App() {
   const [urlList, seturlList] = useState([])
 
   function getFiles() {
-     // 使用 import.meta.glob 获取文件列表
-     const fileList = import.meta.glob("/public/articles/*");
-     const fileNames = Object.keys(fileList).map((filePath) =>
-       filePath.split("/").pop()
-     );
-     console.log('fileNames ', { fileNames })
-     setFiles(fileNames);
+    // 使用 import.meta.glob 获取文件列表
+    const fileList = import.meta.glob("/public/articles/*");
+    const fileNames = Object.keys(fileList).map((filePath) =>
+      filePath.split("/").pop()
+    );
+    console.log('fileNames ', { fileNames })
+    setFiles(fileNames);
   }
 
   function loadFile(name) {
-     // 加载 public 目录中的 init.md 文件
-     fetch(`/articles/${name}`)
-     .then((response) => response.text())
-     .then((t) => {
-      // console.log(t)
-       return t
-     })
-     .then((text) => setMarkdown(text))
-     .catch((error) => console.error("Failed to load markdown file:", error));
+    // 加载 public 目录中的 init.md 文件
+    fetch(`/articles/${name}`)
+      .then((response) => response.text())
+      .then((t) => {
+        // console.log(t)
+        return t
+      })
+      .then((text) => setMarkdown(text))
+      .catch((error) => console.error("Failed to load markdown file:", error));
   }
 
   function loadUrlList() {
     fetch('data.json')
-    .then((response) => response.text())
-    .then((list) => {
-      try {
-        seturlList(
-          JSON.parse(list)
-        )
-      } catch (error) {
-        console.log(error)
-      }
-    })
-    .catch((error) => console.error("Failed to load markdown file:", error));
+      .then((response) => response.text())
+      .then((list) => {
+        try {
+          seturlList(
+            JSON.parse(list)
+          )
+        } catch (error) {
+          console.log(error)
+        }
+      })
+      .catch((error) => console.error("Failed to load markdown file:", error));
   }
 
   return (
@@ -64,16 +64,21 @@ function App() {
 
       <CSVtoJSONConverter />
 
-      <dl>
-        <dt>Beast of Bodmin</dt>
+      <ul>
         {urlList.map(obj => {
           return (
-            <dd key={obj.name}>
+            <li key={obj.name} style={{ marginBlockEnd: 8 }}>
+              <img
+                height={24}
+                width={24}
+                src={obj.icon}
+                style={{ verticalAlign: 'bottom', paddingRight: 8 }}
+              />
               <a href={obj.url} target='_blank'>{obj.name}</a>
-            </dd>
+            </li>
           )
         })}
-      </dl>
+      </ul>
 
       <p>
         {files.map(name => (<a key={name} onClick={() => loadFile(name)}>{name}</a>))}
