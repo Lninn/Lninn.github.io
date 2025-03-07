34797df9cbec2ff712edb989fd10754e66a17a2b
Lninn <thl384458870@outlook.com>
2025-03-07 11:06:23 +0800

    代码重构

--- src/pages/ComponentDemo/index.jsx
+++ src/pages/ComponentDemo/index.jsx
@@ -1,13 +1,10 @@
 import './index.css'
 import ExampleComponent from './Modal/ExampleComponent'
 import ConfirmDialog from './Modal/ConfirmDialog'
-import SelectDemo from './SelectDemo'
 
 export default function ComponentDemo() {
   return (
     <div className="component-demo">
-      <h3>Select 组件</h3>
-      <SelectDemo />
 
       <h1>组件演示</h1>
       <p>这里是组件演示页面，你可以在这里预览和测试各个组件。</p>
